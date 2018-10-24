5ddf53a0f70058ffcffcf87f27dfd925f2a51e6a
Roman Nguyen <march08@outlook.com>
2018-10-24 23:51:15 +0200

    cleanup, update package

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,56 +1,39 @@
-import PropTypes from 'prop-types'
 import React, { PureComponent as Component } from 'react'
 import serialize from 'form-serialize'
-import classnames from 'classnames'
 
 class Form extends Component {
-
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-    children: PropTypes.node,
-    className: PropTypes.string
-  }
-
   static defaultProps = {
     className: null
   }
 
-  constructor(...args) {
-    super(...args)
-    this.getValues = this.getValues.bind(this)
-    this.submit = this.submit.bind(this)
-    this.formSubmit = this.formSubmit.bind(this)
-    this.setRef = this.setRef.bind(this)
-  }
-
-  getValues() {
+  getValues = () => {
     return serialize(this.form, {
       hash: true
     })
   }
 
-  submit(...args) {
+  submit = (...args) => {
     const values = serialize(this.form, {
       hash: true
     })
     return this.props.onSubmit(values, ...args)
   }
 
-  formSubmit(e) {
+  formSubmit = (e) => {
     e.preventDefault()
     this.submit()
   }
 
-  setRef(name) {
+  setRef = (name) => {
     return (component) => {
       this[name] = component
     }
   }
 
   render() {
-    const { className } = this.props
+    const { className, ...rest } = this.props
     return (
-      <form onSubmit={ this.formSubmit } noValidate className={ classnames(className) } ref={ this.setRef('form') }>
+      <form {...rest} onSubmit={ this.formSubmit } noValidate ref={ this.setRef('form') }>
         {this.props.children}
       </form>
     )
